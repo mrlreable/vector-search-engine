1a95e55464077801785ab2089b831edaa9c0cde6
mrlelable <levente.lang@artofinfo.eu>
2023-04-09 19:08:24 +0200

    weaviate client setup

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+)
+
+func main() {
+	cfg := weaviate.Config{
+		Host:   "localhost:8080", // Replace with your endpoint
+		Scheme: "http",
+	}
+	client, err := weaviate.NewClient(cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	schema, err := client.Schema().Getter().Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", schema)
+}
